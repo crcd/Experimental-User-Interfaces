0f62e822b46d83b114838202f1c1b6c03430c588
Sampo Verkasalo <sampo.verkasalo@gmail.com>
2014-03-20 16:20:08 +0200

    Add simple control and curving scripts

---
+++ Curling/Assets/SpacebarThrow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpacebarThrow : MonoBehaviour {
+  public float velocity;
+  public float angularVelocity;
+  public void Update() {
+    if (Input.GetKeyDown ("space")) {
+      rigidbody.velocity = new Vector3(0,0,velocity);
+      rigidbody.angularVelocity = new Vector3(0,angularVelocity,0);
+    }
+  }
+}
