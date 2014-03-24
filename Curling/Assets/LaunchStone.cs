cca3bb0cda99afdaf0b26a278ae34d556cb167d9
Juha Rantanen <jmranta1@niksula.hut.fi>
2014-03-24 09:15:30 +0200

    Basic launch functionality with PSWand.

---
+++ Curling/Assets/LaunchStone.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchStone : MonoBehaviour {
+
+	private RUISPSMoveWand moveWand;
+	private FollowStoneCamera camera;
+
+	private Rigidbody stoneBody;
+	public float velocity;
+	public float angularVelocity;
+
+	// Use this for initialization
+	void Start () {
+
+		moveWand = gameObject.GetComponent<RUISPSMoveWand> ();
+
+		camera = GameObject.Find("RUISCamera").GetComponent<FollowStoneCamera>();
+
+		stoneBody = GameObject.Find("StoneRed").rigidbody;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!stoneBody) return;
+
+		if (moveWand.triggerButtonWasPressed) {
+
+			stoneBody.velocity = new Vector3(0,0,velocity);
+			// Debug.Log("Trigger button was pressed!!!");
+		}
+
+		if (moveWand.triggerButtonWasReleased) {
+			//Debug.Log("Trigger button was released!!!");
+
+			camera.follow = false;
+			stoneBody.angularVelocity = new Vector3(0,angularVelocity,0);
+
+		}
+
+
+	
+	}
+}
