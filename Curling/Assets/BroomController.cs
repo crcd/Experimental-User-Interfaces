df6cf0c5f4e57e8c659a116c8376a86ab0b19a61
Juha Rantanen <jmranta1@niksula.hut.fi>
2014-05-13 17:48:31 +0300

    Scene updated. BroomController fixes.

--- Curling/Assets/BroomController.cs
+++ Curling/Assets/BroomController.cs
@@ -17,24 +17,37 @@ public class BroomController : MonoBehaviour {
 	private float[] leftBroomArea;
 	private float[] centerBroomArea;
 	private float[] rightBroomArea;
+
+	private float[] tempLeftBroom;
+	private float[] tempCenterBroom;
+	private float[] tempRightBroom;
+
 	private int history = 50;
 	
 	private float prevX = 0;
 	private float currX = 0;
 	private float distX = 0;
+	private float currZ = 0;
 	
 	private int dirX = 1;
 	public float offsetCenterX;
-	public float offsetAmplitude;
+	public float offsetCenterZ;
+	public float offsetAmplitudeX; //2.5
+	public float offsetAmplitudeZ;
 	public float rotationAmount;
 	public float moveToBroomFactor;
 	private float minX;
 	private float maxX;
+	private float minZ;
+	private float maxZ;
+	private float tempZ;
 	private float tempX;
 	
 	public float distThreshold;
 	
+	private float leftFriction = 0.0001f; // prevent division by zero
 	private float centerFriction = 0.0001f; // prevent division by zero
+	private float rightFriction = 0.0001f; // prevent division by zero
 	
 	private float broomingCoefficient = 1.0f/30.0f;
 	
@@ -50,17 +63,29 @@ public class BroomController : MonoBehaviour {
 		leftBroomArea = new float[history];
 		rightBroomArea = new float[history];
 		centerBroomArea = new float[history];
+		tempLeftBroom = new float[history];
+		tempRightBroom = new float[history];
+		tempCenterBroom = new float[history];
 		
 	}
 	
-	public void updateBroom(float prevX, float currX, float distX) {
+	public void updateBroom(float prevX, float currX, float distX, float currZ) {
 		this.prevX = prevX;
 		this.currX = currX;
-		this.distX = distX;	
+		this.distX = distX;
+		this.currZ = currZ;
 	}
-	
+
+	public float getLeftFriction() {
+		return this.leftFriction;
+	}
+
 	public float getCenterFriction() {
 		return this.centerFriction;
+	}
+
+	public float getRightFriction() {
+		return this.rightFriction;
 	}
 	
 	void FixedUpdate () {
@@ -90,24 +115,34 @@ public class BroomController : MonoBehaviour {
 
 		// Broom should be adjusted 
 		
-		minX = offsetCenterX - offsetAmplitude;
-		maxX = offsetCenterX + offsetAmplitude;
+		minX = offsetCenterX - offsetAmplitudeX;
+		maxX = offsetCenterX + offsetAmplitudeX;
+
+		minZ = offsetCenterZ - offsetAmplitudeZ;
+		maxZ = offsetCenterZ + offsetAmplitudeZ;
+
 //		
 //		if (offset.z > maxX) offset.z = maxX;
 //		if (offset.z < minX) offset.z = minX;
 
 		tempX = currX;
+		tempZ = currZ;
 
 		if (tempX < minX) tempX = minX;
 		if (tempX > maxX) tempX = maxX;
+		if (tempZ < minZ) tempZ = minZ;
+		if (tempZ > maxZ) tempZ = maxZ;
 
-		offset.z = (tempX - offsetCenterX) * moveToBroomFactor;
+		offset.z = -(tempX - offsetCenterX) * moveToBroomFactor;
+
+		offset.x = (tempZ - offsetCenterZ) * moveToBroomFactor;
 
 
 		
 		bodyOffset = offset;
 		
-		float rotX = ((tempX - offsetCenterX) / offsetAmplitude) * -rotationAmount;
+		float rotX = ((tempX - offsetCenterX) / offsetAmplitudeX) * -rotationAmount;
+		float rotZ = ((tempZ - offsetCenterZ) / offsetAmplitudeZ) * -rotationAmount;
 		
 		//gameObject.transform.position = broomerBody.position + offset;
 		
@@ -129,27 +164,57 @@ public class BroomController : MonoBehaviour {
 		
 		//Debug.Log (rotX);
 		
-		gameObject.transform.eulerAngles = new Vector3 (rotX, 90, 0);
+		gameObject.transform.eulerAngles = new Vector3 (-rotX, -90, rotZ);
 		
 		//Debug.Log ("PrevX: " + prevX + " CurrX: " + currX + " DistX: " + distX);
-		
-		var tempBroom = new float[history];
-		tempBroom [0] = distX;
+
+
+
+
+		if (offset.z >= -0.5 && offset.z < 0.3*(-0.5)) {
+			// Left area
+			tempLeftBroom[0] = distX;
+			tempCenterBroom[0] = 0.0f;
+			tempRightBroom[0] = 0.0f;
+		} else if (offset.z >= 0.3*(-0.5) && offset.z < 0.3*0.5) {
+			// Center area
+			tempLeftBroom[0] = 0.0f;
+			tempCenterBroom[0] = distX;
+			tempRightBroom[0] = 0.0f;
+		} else {
+			// Right area
+			tempLeftBroom[0] = 0.0f;
+			tempCenterBroom[0] = 0.0f;
+			tempRightBroom[0] = distX;
+		}
+
 		for ( int i = 1; i < history; i++ ) {
-			tempBroom[i] = centerBroomArea[i-1];
+			tempLeftBroom[i] = tempLeftBroom[i-1];
+			tempCenterBroom[i] = tempCenterBroom[i-1];
+			tempRightBroom[i] = tempRightBroom[i-1];
 		}
-		centerBroomArea = tempBroom;
 		
-		float sumArea = 0.0001f; // prevent zero division
+		float sumAreaLeft = 0.0001f; // prevent zero division
+		float sumAreaCenter = 0.0001f; // prevent zero division
+		float sumAreaRight = 0.0001f; // prevent zero division
 		for ( int i = 0; i < history; i++ ) {
-			sumArea = sumArea + tempBroom[i];
+			sumAreaLeft = sumAreaLeft + tempLeftBroom[i];
+			sumAreaCenter = sumAreaCenter + tempCenterBroom[i];
+			sumAreaRight = sumAreaRight + tempRightBroom[i];
 		}
-		if (sumArea > 30) {
-			sumArea = 30;
+		if (sumAreaLeft > 30) {
+			sumAreaLeft = 30;
+		}
+		if (sumAreaCenter > 30) {
+			sumAreaCenter = 30;
+		}
+		if (sumAreaRight > 30) {
+			sumAreaRight = 30;
 		}
 		// sumArea varies now between 0 and 30
-		centerFriction = sumArea * broomingCoefficient;
-		
+		leftFriction = sumAreaLeft * broomingCoefficient;
+		centerFriction = sumAreaCenter * broomingCoefficient;
+		rightFriction = sumAreaRight * broomingCoefficient;
 		
 		
 		//Debug.Log ("sum of area: " + sumArea);
