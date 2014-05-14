13dcbddc91989a96fe9a820b2c00d20a2c2fe4ed
Kaisu <kaisu.olander@hotmail.com>
2014-05-14 16:04:25 +0300

    Remove rotation from arrows

---
+++ Curling/Assets/DeleteRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteRotation : MonoBehaviour {
+	
+	private GameObject rotater;
+	private float yrot;
+	
+	void Start () {
+		rotater = GameObject.Find ("RotationHelper");
+	}
+
+	void FixedUpdate () {
+		yrot = rotater.transform.parent.rotation.y;
+		rotater.transform.rotation = Quaternion.Euler(0, -1.0f * yrot, 0);
+	}
+}
